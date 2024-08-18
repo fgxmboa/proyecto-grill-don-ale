b432a8e1e409d40a06d84e7508195ebb93bf4cd1
Francisco <franciscogamboa03@hotmail.com>
2024-08-18 00:16:16 -0600

    Added controllers and routes for Cliente, Pedido, and Menu entities

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\PedidoController;
+use App\Http\Controllers\MenuController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('clientes', [ClienteController::class, 'index']) ->name('cliente.index');
+Route::get('pedido', [PedidoController::class, 'index']) ->name('pedido.index');
+Route::get('menu', [MenuController::class, 'index']) ->name('menu.index');
+
