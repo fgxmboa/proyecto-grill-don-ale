a61ca134beceabcd9b2c414fca77cfbccd585ee3
Francisco <franciscogamboa03@hotmail.com>
2024-08-25 14:44:29 -0600

    Added migration for creating 'clientes' table

---
+++ database/migrations/2024_08_25_203149_create_clientes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id('idClientes');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('cedula');
+            $table->string('email');
+            $table->string('telefono');
+            $table->string('direccion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
